402b98b6c45d615c64f80f9bab9984586a135b9a
earl-stephens <stephensearl@hotmail.com>
2019-07-21 15:11:03 -0600

    receiving api call

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
+require './stopwatch'
 class Timer < Sinatra::Base
+  attr_reader :latest_time
 
-  get '/timer' do
-    "hello world"
+  put '/timer' do
+    # "hello world"
+    request.body.rewind
+    @latest_time = JSON.parse([request.body.read].to_json)
+    sw = Stopwatch.new(@latest_time)
+    sw.find_diff
   end
-
-  # post 'timer/' do
-  #   binding.pry
+  # #
+  # get '/display_time' do
+  #   # binding.pry
+  #   "hello"
   # end
 
 end
--- stopwatch.rb
+++ stopwatch.rb
@@ -1,5 +1,12 @@
 class Stopwatch
+  attr_reader :latest_time
 
-  
+  def initialize(time)
+    @latest_time = time
+  end
+
+  def find_diff
+    binding.pry
+  end
 
 end
