fd3e4a6610cfc8457aa9d3a284a204f598975f95
earl-stephens <stephensearl@hotmail.com>
2019-07-22 19:17:01 -0600

    getting ready to work on connection to rails

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './stopwatch'
+require 'json'
 # require 'faraday'
 class Timer < Sinatra::Base
   attr_reader :latest_time
@@ -11,18 +12,31 @@ class Timer < Sinatra::Base
     # "hello world"
     request.body.rewind
     @latest_time = JSON.parse([request.body.read].to_json)
+    # binding.pry
     sw = Stopwatch.new(@latest_time)
-    sw.find_diff
-  end
+    # sw.find_diff
 
-  get '/display_time' do
-    data = {"sinatra_data": "1234567890"}
+    data = {"sinatra_data": "#{sw.find_diff}"}
     data = data.to_json
+  # binding.pry
     Faraday.put('http://127.0.0.1:3000/receiver') do |f|
               f.body =  data
               f.headers["Content-Type"] = "application/json"
               f.headers["Content-Length"] = "16"
               end
   end
+
+  get '/timer2.json' do
+  # binding.pry
+  request.body.rewind
+  @latest_time = JSON.parse([request.body.read].to_json)
+  # binding.pry
+  sw = Stopwatch.new(@latest_time)
+  # sw.find_diff
+  # binding.pry
+  content_type :json
+  {:key1 => "#{sw.find_diff}"}.to_json
+
+end
 
 end
--- stopwatch.rb
+++ stopwatch.rb
@@ -3,11 +3,11 @@ class Stopwatch
   attr_reader :latest_time
 
   def initialize(time)
-    @latest_time = time
+    @latest_time = JSON.parse(time.first)["time_data"].to_i
   end
 
   def find_diff
-    binding.pry
+    Time.now - @latest_time
   end
 
 end
